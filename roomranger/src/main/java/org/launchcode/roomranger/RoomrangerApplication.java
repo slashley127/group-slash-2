195384b36b23791be2111e2f38de1abd5e7ba9de
LunaLiu4 <luna.liu829@gmail.com>
2024-01-10 17:20:02 -0600

    initial

---
+++ roomranger/src/main/java/org/launchcode/roomranger/RoomrangerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.roomranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomrangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomrangerApplication.class, args);
+	}
+
+}
