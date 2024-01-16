7989bcdcfcdd927ab47ff89f4dae6558893adb48
Your Name <you@example.com>
2024-01-16 13:52:31 -0600

    testing

--- roomranger/src/main/java/org/launchcode/roomranger/RoomrangerApplication.java
+++ roomranger/src/main/java/org/launchcode/roomranger/RoomrangerApplication.java
@@ -1,13 +1,18 @@
 package org.launchcode.roomranger;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.annotation.Validated;
 
 @SpringBootApplication
 public class RoomrangerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomrangerApplication.class, args);
+//		nkjnkjnjknk
 	}
 
+
 }
+
