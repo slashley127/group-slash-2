bf38b8da1c59a763afe0a24ae6d739493e7a722e
MuntoniFrancesca <francesca.muntoni@live.com>
2024-01-19 15:12:36 -0600

    created manager homepage template

--- roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
@@ -1,7 +1,15 @@
 package org.launchcode.roomranger.controllers;
-
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ManagerHomepageController {
+
+    @GetMapping
+    public String home(){
+        return "managerhomepage";
+    }
+
 }
