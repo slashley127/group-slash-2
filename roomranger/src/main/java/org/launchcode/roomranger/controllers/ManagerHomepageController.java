3f4c14f6492baf85efa23ca2972205328fbea81f
MuntoniFrancesca <francesca.muntoni@live.com>
2024-01-21 17:43:49 -0600

    created manager homepage controller and navbar

--- roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
@@ -3,13 +3,21 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("manager")
 public class ManagerHomepageController {
 
-    @GetMapping
-    public String home(){
-        return "managerhomepage";
+
+    @GetMapping("index")
+    public String renderManagerHomepage(Model model){
+        return "manager/index";
+    }
+
+    @GetMapping("roomattendantlist")
+    public String renderListOfRoomAttendants(Model model){
+        return "manager/roomattendantlist";
     }
 
 }
