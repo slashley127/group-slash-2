2c3ef472b4f71e7c71d6a08277eaa9e8f076f3a0
MuntoniFrancesca <francesca.muntoni@live.com>
2024-01-23 08:59:39 -0600

    started adding autowired repositories to manager controller

--- roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
@@ -1,14 +1,14 @@
 package org.launchcode.roomranger.controllers;
+import org.launchcode.roomranger.data.RoomAttendantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("manager")
 public class ManagerHomepageController {
-
 
     @GetMapping
     public String renderManagerHomepage(Model model){
