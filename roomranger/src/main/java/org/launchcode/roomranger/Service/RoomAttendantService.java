1f4821448cad74006a57958d5af0e09d08d23d91
Your Name <you@example.com>
2024-01-23 13:14:10 -0600

    commit 1

---
+++ roomranger/src/main/java/org/launchcode/roomranger/Service/RoomAttendantService.java
@@ -0,0 +1,30 @@
+//package org.launchcode.roomranger.Service;
+//
+//import org.launchcode.roomranger.Repository.RoomAttendantRepository;
+//import org.launchcode.roomranger.models.DTO.AddRoomAttendantDTO;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class RoomAttendantService {
+//
+//    private static RoomAttendantRepository roomAttendantRepository;
+//
+//    public String save(AddRoomAttendantDTO roomAttendant) {
+//        roomAttendantRepository.save(roomAttendant);
+//        return "Success";
+//    }
+//
+//    //private RoomAttendant roomAttendant;
+////    public static List<RoomAttendant> displayAllRoomAttendants() {
+////        return (List<RoomAttendant>) roomAttendantRepository.findAll();
+////    }
+//
+////    public String processRoomAttendantForm(RoomAttendant roomAttendant){
+////         roomAttendantRepository.save(roomAttendant);
+////        return"roomAttendant";
+//
+//
+//    //}
+//}
+//
