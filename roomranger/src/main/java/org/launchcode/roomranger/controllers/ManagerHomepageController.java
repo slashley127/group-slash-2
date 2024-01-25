b4611a2bfe5862b67bc968b66abd8f3da62efaea
LunaLiu4 <luna.liu829@gmail.com>
2024-01-25 15:49:12 -0600

    add managerHomepageController

---
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/ManagerHomepageController.java
@@ -0,0 +1,37 @@
+package org.launchcode.roomranger.controllers;
+import org.launchcode.roomranger.data.RoomAttendantRepository;
+import org.launchcode.roomranger.data.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("manager")
+public class ManagerHomepageController {
+
+    @Autowired
+    private RoomAttendantRepository roomAttendantRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping
+    public String renderManagerHomepage(Model model){
+        model.addAttribute("rooms", roomRepository.findAll());
+        return "manager/index";
+    }
+
+    @GetMapping("roomattendantlist")
+    public String renderListOfRoomAttendants(Model model){
+        return "manager/roomattendantlist";
+    }
+
+    @GetMapping("assignroom")
+    public String renderAssignRoomForm(Model model) {
+        return "manager/assignroom";
+    }
+}
