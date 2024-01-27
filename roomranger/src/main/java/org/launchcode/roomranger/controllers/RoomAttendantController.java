30ca28af512fc066422c15d15f65acd27c55a3e1
Your Name <you@example.com>
2024-01-26 18:25:10 -0600

    Add Room Attendant page and About Us page done!

--- roomranger/src/main/java/org/launchcode/roomranger/controllers/RoomAttendantController.java
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/RoomAttendantController.java
@@ -1,30 +1,64 @@
 package org.launchcode.roomranger.controllers;
 
-import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.launchcode.roomranger.data.ManagerRepository;
 import org.launchcode.roomranger.data.RoomAttendantRepository;
-import org.launchcode.roomranger.data.RoomRepository;
-import org.launchcode.roomranger.data.UserRepository;
-import org.launchcode.roomranger.models.Room;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.launchcode.roomranger.models.DTO.AddRoomAttendantDTO;
+import org.launchcode.roomranger.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.launchcode.roomranger.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.launchcode.roomranger.models.RoomAttendant;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
-@RequestMapping("attendant")
+@RequestMapping(value = "roomAttendant")
 public class RoomAttendantController {
 
     @Autowired
-    private RoomRepository roomRepository;
-
+    private RoomAttendantRepository roomAttendantRepository;
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
-    private RoomAttendantRepository roomAttendantRepository;
+    private ManagerRepository managerRepository;
+
     @GetMapping
-    public String displayRoomsAssigned(Model model, HttpSession session){
-        return "attendant/index";
+    public String displayAllRoomAttendants(Model model) {
+        model.addAttribute("title", "All Room Attendants");
+        model.addAttribute("roomAttendant", roomAttendantRepository.findAll());
+        //System.out.println("Room attendant list size:"+roomAttendantRepository.findAll().size());
+        return "roomAttendant/index";
+    }
+
+
+    @GetMapping("add")
+    public String addRoomAttendant(Model model) {
+        model.addAttribute("title", "Add Room Attendant");
+        model.addAttribute(new RoomAttendant());
+        return "roomAttendant/add";
+    }
+
+    @PostMapping("add")
+    public String processRoomAttendantForm(@ModelAttribute @Valid RoomAttendant newRoomAttendant, Errors errors, Model model, RedirectAttributes redirectAttributes) {
+        if (errors.hasErrors()) {
+            return "roomAttendant/add";
+        }else{
+            model.addAttribute("title", "Add a Room Attendant");
+            redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+            roomAttendantRepository.save(newRoomAttendant);
+            System.out.println("Successfully saved entity");
+            return "redirect:/roomAttendant";
+        }
+
     }
 }
+
+
+
+
+
+
--- roomranger/src/main/java/org/launchcode/roomranger/models/RoomAttendant.java
+++ roomranger/src/main/java/org/launchcode/roomranger/models/RoomAttendant.java
@@ -10,16 +10,13 @@ import java.util.List;
 
 @Entity
 public class RoomAttendant extends AbstractEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(insertable=false, updatable=false)
     private int id;
-
     @NotBlank(message = "First name is required")
     @Column(length = 30)
     private String firstName;
-
     @NotBlank(message = "Last name is required")
     private String lastName;
     @Column(unique = true, length = 15)
@@ -30,63 +27,12 @@ public class RoomAttendant extends AbstractEntity {
     private Manager manager;
     @OneToOne(cascade = CascadeType.ALL)
     private RoomAttendantUser roomAttendantUser;
-    private String gender;
+    private String pronoun;
     private String phoneNumber;
     private List<WorkingDays> workingDays = new ArrayList<>();
-    public String getGender() {return gender;}
-
-    public void setGender(String gender) { this.gender = gender; }
-
     @Column(unique = true)
     private String email;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public List<WorkingDays> getWorkingDays() {
-        return workingDays;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
     private String notes;
-
-    public RoomAttendant() {
-
-    }
-
-
     public String getFirstName() {return firstName;}
 
     public void setFirstName(String firstName) {this.firstName = firstName;}
@@ -94,25 +40,64 @@ public class RoomAttendant extends AbstractEntity {
     public String getLastName() {return lastName;}
 
     public void setLastName(String lastName) {this.lastName = lastName;}
-
     public void setId(int id) {
         this.id = id;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-
-
     }
-
+    public int getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public List<WorkingDays> getWorkingDays() {
+        return workingDays;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setWorkingDays(List<WorkingDays> workingDays) {
         this.workingDays = workingDays;
     }
+    public Manager getManager() {return manager;}
 
+    public void setManager(Manager manager) {this.manager = manager;}
+
+    public RoomAttendantUser getRoomAttendantUser() {return roomAttendantUser;}
+
+    public void setRoomAttendantUser(RoomAttendantUser roomAttendantUser) {this.roomAttendantUser = roomAttendantUser;}
+
+    public String getPronoun() {return pronoun;}
+
+    public void setPronoun(String pronoun) {this.pronoun = pronoun;}
+
+
+    public RoomAttendant() {
+
+    }
     public RoomAttendant ( int id, String firstName, String lastName, String email, String phoneNumber, List<WorkingDays> workingDays, String username, String password, RoomAttendantUser roomAttendantUser, String notes) {
         super();
         this.id = id;
@@ -120,35 +105,11 @@ public class RoomAttendant extends AbstractEntity {
         this.lastName = lastName;
         this.email= email;
         this.phoneNumber= phoneNumber;
-        this.manager = manager;
         this.workingDays = workingDays;
         this.username= username;
         this.password=password;
         this.roomAttendantUser=roomAttendantUser;
         this.notes=notes;
-
-    }
-
-    public RoomAttendantUser getRoomAttendantUser() {
-        return roomAttendantUser;
-    }
-
-
-
-    public String getusername() {return username;}
-
-    public void setusername(String username) {this.username = username;}
-
-    public void setpassword(String password) {this.password = password;}
-
-    public Manager getManager() {return manager;}
-
-    public void setManager(Manager manager) {this.manager = manager;}
-
-    public RoomAttendantUser roomAttendantUser() {return roomAttendantUser();}
-
-    public void setRoomAttendantUser(RoomAttendantUser roomAttendantUser) {
-        this.roomAttendantUser = roomAttendantUser;
     }
 }
 
