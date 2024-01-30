c8c90bb56ddbc17dc938fe7e5c58766bb0e0d36a
Your Name <you@example.com>
2024-01-30 10:24:58 -0600

    Add Room Attendant page and About Us page done!

--- roomranger/src/main/java/org/launchcode/roomranger/controllers/AddAttendantController.java
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/AddAttendantController.java
@@ -1,12 +1,10 @@
 package org.launchcode.roomranger.controllers;
 
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.launchcode.roomranger.data.ManagerRepository;
 import org.launchcode.roomranger.data.RoomAttendantRepository;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.launchcode.roomranger.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.launchcode.roomranger.models.RoomAttendant;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.launchcode.roomranger.models.RoomAttendantProfile;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "roomAttendant")
-public class RoomAttendantController {
+public class AddAttendantController {
 
     @Autowired
     private RoomAttendantRepository roomAttendantRepository;
@@ -105,26 +99,7 @@ public class RoomAttendantController {
         return "redirect:/roomAttendant";
     }
 
-    @GetMapping("profile")
-    public String getProfile(Model model) {
-        model.addAttribute("title", "Add Room Attendant");
-        model.addAttribute(new RoomAttendant());
-        return "roomAttendant/profile";
-    }
 
-    @PostMapping("/profile/{id}")
-    public String viewProfile(@PathVariable("id") int id, Model model)
-    {
-        RoomAttendant roomAttendant = roomAttendantRepository.findById(id);
-        //List<RoomAttendant> courses = roomAttendant.getRoomAttendant();
-//        if(roomAttendant.isEmpty()) {
-//            return "redirect:/students/" + id + "/addCourses";
-//        }
-       // model.addAttribute("remove_id", id);
-        //roomAttendantRepository.save(roomAttendant);
-        model.addAttribute("roomAttendant", roomAttendant);
-        return "profile";
-    }
 
 }
     
--- roomranger/src/main/java/org/launchcode/roomranger/controllers/AttendantprofileController.java
+++ roomranger/src/main/java/org/launchcode/roomranger/controllers/AttendantprofileController.java
@@ -1,29 +1,40 @@
-//package org.launchcode.roomranger.controllers;
-//
-//import org.launchcode.roomranger.data.ManagerRepository;
-//import org.launchcode.roomranger.data.RoomAttendantRepository;
-//import org.launchcode.roomranger.data.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("profile")
-//public class AttendantprofileController {
-//    @Autowired
-//    private RoomAttendantRepository roomAttendantRepository;
-//    @Autowired
-//    private UserRepository userRepository;
-//    @Autowired
-//    private ManagerRepository managerRepository;
-//
-//    @GetMapping
-//    public String viewProfile(Model model){
-//        model.addAttribute("title","View Profile")
-//        return "profile";
-//    }
-//
-//}
+package org.launchcode.roomranger.controllers;
+
+import org.launchcode.roomranger.data.ManagerRepository;
+import org.launchcode.roomranger.data.RoomAttendantRepository;
+import org.launchcode.roomranger.data.UserRepository;
+import org.launchcode.roomranger.models.RoomAttendant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("roomAttendant")
+public class AttendantprofileController {
+    @Autowired
+    private RoomAttendantRepository roomAttendantRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ManagerRepository managerRepository;
+
+    @GetMapping("profile")
+    public String getProfile(Model model) {
+        model.addAttribute("title", "Add Room Attendant");
+        model.addAttribute(new RoomAttendant());
+        return "roomAttendant/profile";
+    }
+
+    @PostMapping("/profile/{id}")
+    public String viewProfile(@PathVariable("id") int id, Model model, RedirectAttributes redirectAttributes)
+    {
+        RoomAttendant roomAttendant = roomAttendantRepository.findById(id);
+        model.addAttribute("roomAttendant", roomAttendant);
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
+        return "profile";
+    }
+
+}
