cce4397acc89523a97040ecfec86cd570d3986d8
MuntoniFrancesca <francesca.muntoni@live.com>
2024-02-05 14:35:13 -0600

    completed form and connected enand room numbers to form

--- roomranger/frontend/src/App.js
+++ roomranger/frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import NavBar from './components/NavBar';
 import Home from './components/rooms/Home';
+import HomeManager from './components/manager/HomeManager';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import AddRoom from './components/rooms/AddRoom';
 import EditRoom from './components/rooms/EditRoom';
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           {/* give path /addroom to the AddRoom page */}
           <Route exact path="/rooms/addroom" element={<AddRoom />} />
+          <Route exact path="/manager" element={<HomeManager />} />
           <Route exact path="/rooms" element={<Home />} />
           <Route exact path="/rooms/editroom/:id" element={<EditRoom/>} />
           <Route exact path="/rooms/viewroom/:id" element={<ViewRoom/>} />
