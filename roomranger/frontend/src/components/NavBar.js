13cd55778715cab04d50b2351b06918f1f4583b0
samatakarka <samata.karka@gmail.com>
2024-02-09 09:45:38 -0600

    linked with luna's

--- roomranger/frontend/src/components/NavBar.js
+++ roomranger/frontend/src/components/NavBar.js
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <div>
-            <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <div className='nbar'>
+            <nav class="navbar navbar-expand-sm navbar-dark">
+            <div className='text-left'>
+
+                    <a href='https://roomranger.com' className='navbar-brand '><h2>RoomRanger</h2></a>
+                    </div>
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="#">Home Page</a>
@@ -16,7 +20,7 @@ export default function NavBar() {
                         <a class="nav-link" href="#">Assign Room</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Room Attendant</a>
+                    <Link class="nav-link active" aria-current="page" to="/attendantListComponent">Room Attendant</Link>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Leave Request</a>
