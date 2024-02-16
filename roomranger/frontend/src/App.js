3e563c2d737cb029ad09fb87987a78c54b498ddc
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2024-02-15 23:41:35 -0600

    check

--- roomranger/frontend/src/App.js
+++ roomranger/frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home.js";
@@ -11,6 +12,26 @@ import ViewRoom from "./components/rooms/ViewRoom";
 import HomeRoom from "./components/rooms/HomeRoom";
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+
+//import NavBar from './components/NavBar';
+import AttendantListComponent from './components/roomAttendants/AttendantListComponent';
+import AddAttendant from './components/roomAttendants/AddAttendant';
+import Update from './components/roomAttendants/Update';
+import Profile from './components/roomAttendants/Profile';
+//import { Header } from './components/Header';
+//import { Footer } from './components/Footer';
+//import LoginPage from './components/loginPage/LoginPage';
+// import ManagerHomePage from './components/ManagerHomePage';
+//import Homepage from './components/manager/Homepage';
+//import ErrorComponent from './components/ErrorComponent';
+
+//import RouteHome from './components/Route';
+ // import HomeAttendant from './components/attendantRolePage/HomeAttendant';
+// import LeaveList from './components/leaveRequest/LeaveList';
+import LeaveForm from './components/leaveRequest/LeaveForm';
+import LeaveList from './components/leaveRequest/LeaveList';
+import EditRequest from './components/leaveRequest/EditRequest';
+//import AssignRoom from './components/manager/AssignRoom';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -39,15 +60,15 @@ function App() {
                 <Route exact path="/rooms/editRoom" element={<EditRoom />} />
                 <Route exact path="/rooms/viewRoom" element={<ViewRoom />} />
                 <Route exact path="/rooms/HomeRoom" element={<HomeRoom />} />
-                
-                </Routes>
+
+              </Routes>
             </Col>
             <Col md={6}>
               {/* Content for the second column */}
             </Col>
           </Row>
         </Container>
-      </Router>
+        </Router>
     </div>
   );
 }
