5c741172dd257213cc3d932d0f4fda775a7d71fd
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2024-02-16 11:18:13 -0600

    App.js

--- roomranger/frontend/src/App.js
+++ roomranger/frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home.js";
@@ -13,25 +12,17 @@ import HomeRoom from "./components/rooms/HomeRoom";
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
-//import NavBar from './components/NavBar';
+import NavBar from './components/NavBar';
 import AttendantListComponent from './components/roomAttendants/AttendantListComponent';
 import AddAttendant from './components/roomAttendants/AddAttendant';
 import Update from './components/roomAttendants/Update';
 import Profile from './components/roomAttendants/Profile';
-//import { Header } from './components/Header';
-//import { Footer } from './components/Footer';
-//import LoginPage from './components/loginPage/LoginPage';
-// import ManagerHomePage from './components/ManagerHomePage';
-//import Homepage from './components/manager/Homepage';
-//import ErrorComponent from './components/ErrorComponent';
-
-//import RouteHome from './components/Route';
- // import HomeAttendant from './components/attendantRolePage/HomeAttendant';
-// import LeaveList from './components/leaveRequest/LeaveList';
+import HomeManager from './components/manager/HomeManager';
 import LeaveForm from './components/leaveRequest/LeaveForm';
 import LeaveList from './components/leaveRequest/LeaveList';
 import EditRequest from './components/leaveRequest/EditRequest';
-//import AssignRoom from './components/manager/AssignRoom';
+import AssignRoom from './components/manager/AssignRoomForm';
+import EditAssignedRoom from './components/manager/EditAssignedRoom';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -48,27 +39,28 @@ function App() {
     <div className="App">
       <button onClick={toggleTheme}>Dark/Light</button>
       <Router>
-        <Container fluid>
-          <Row>
-            <Col md={6}>
-              <Routes>
-                <Route exact path="/registration" element={<Registration />} />
-                <Route exact path="/loginPage" element={<LoginPage />} />
-                <Route exact path="/about" element={<About />} /> 
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/rooms/addRoom" element={<AddRoom />} />
-                <Route exact path="/rooms/editRoom" element={<EditRoom />} />
-                <Route exact path="/rooms/viewRoom" element={<ViewRoom />} />
-                <Route exact path="/rooms/HomeRoom" element={<HomeRoom />} />
-
-              </Routes>
-            </Col>
-            <Col md={6}>
-              {/* Content for the second column */}
-            </Col>
-          </Row>
-        </Container>
-        </Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/registration" element={<Registration />} />
+          <Route exact path="/loginPage" element={<LoginPage />} />
+          <Route exact path="/about" element={<About />} /> 
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/rooms/addRoom" element={<AddRoom />} />
+          <Route exact path="/rooms/editRoom/:id" element={<EditRoom />} />
+          <Route exact path="/rooms/viewRoom" element={<ViewRoom />} />
+          <Route exact path="/rooms/HomeRoom" element={<HomeRoom />} />
+          <Route exact path="/attendantListComponent" element={<AttendantListComponent />} />
+          <Route exact path="/addAttendant" element={<AddAttendant />} />
+          <Route exact path="/update/:id" element={<Update />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+          <Route exact path="/assignedrooms" element={<HomeManager />} />
+          <Route exact path="/assignedrooms/assignroomform" element={<AssignRoom />} />
+          <Route exact path="/assignedrooms/editassignedroom/:id" element={<EditAssignedRoom />} />
+          <Route exact path="/leaveform" element={<LeaveForm />} />
+          <Route exact path="/leavelist" element={<LeaveList />} />
+          <Route exact path="/editrequest" element={<EditRequest />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- roomranger/frontend/src/components/manager/AssignRoomForm.js
+++ roomranger/frontend/src/components/manager/AssignRoomForm.js
@@ -1,4 +1,3 @@
-cd
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
