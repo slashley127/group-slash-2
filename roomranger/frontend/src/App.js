8ab3c2ce19e19bee1f749e3b9f3c9aa7415a7d28
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2024-02-17 22:24:45 -0600

    changes made

--- roomranger/frontend/src/App.js
+++ roomranger/frontend/src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import HomeRoom from './components/rooms/HomeRoom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddRoom from "./components/rooms/AddRoom";
-import EditRoom from "./components/rooms/EditRoom";
-import ViewRoom from "./components/rooms/ViewRoom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddRoom from './components/rooms/AddRoom';
+import EditRoom from './components/rooms/EditRoom';
+import ViewRoom from './components/rooms/ViewRoom';
 import AssignRoom from './components/manager/AssignRoomForm';
 import EditAssignedRoom from './components/manager/EditAssignedRoom';
 import AttendantListComponent from './components/roomAttendants/AttendantListComponent';
@@ -13,7 +13,7 @@ import AddAttendant from './components/roomAttendants/AddAttendant';
 import Update from './components/roomAttendants/Update';
 import Profile from './components/roomAttendants/Profile';
 import { Footer } from './components/Footer';
-import LoginPage from "./components/loginPage/LoginPage";
+import LoginPage from './components/loginPage/LoginPage';
 import HomeManager from './components/manager/HomeManager';
 import ErrorComponent from './components/ErrorComponent';
 import RouteHome from './components/Route';
@@ -37,10 +37,10 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div className="mode-toggle" onClick={toggleTheme}>
+    <div className='App'>
+      <div className='mode-toggle' onClick={toggleTheme}>
         <div className={`slider ${isDarkMode ? 'dark' : 'light'}`} />
-         <span className="mode-text">{isDarkMode ? 'Light' : 'Dark'}</span>
+         <span className='mode-text'>{isDarkMode ? 'Light' : 'Dark'}</span>
       </div>
       <Router>
          <Routes>
@@ -57,32 +57,32 @@ function App() {
                     <Route path='login' element={<LoginPage />} />
                     <Route path='about' element={<About />} />
                   {/* </Route> */}
-                  <Route path="/landing" element={<NavBar />} >
+                  <Route path='/landing' element={<NavBar />} >
                     {/* <Route path='assignedrooms' element={<RouteHome />}> */}
                       <Route index element={<HomeManager />} />
-                      <Route path="assignroomform" element={<AssignRoom />} />
-                      <Route exact path="editassignedroom/:id" element={<EditAssignedRoom />} />
+                      <Route path='assignroomform' element={<AssignRoom />} />
+                      <Route exact path='editassignedroom/:id' element={<EditAssignedRoom />} />
                     {/* </Route> */}
-                    <Route path="rooms" element={<RouteHome />}>
+                    <Route path='rooms' element={<RouteHome />}>
                       <Route index element={<HomeRoom />} />
-                      <Route path="addroom" element={<AddRoom />} />
-                      <Route path="editroom/:id" element={<EditRoom />} />
-                      <Route path="viewroom/:id" element={<ViewRoom />} />
+                      <Route path='addroom' element={<AddRoom />} />
+                      <Route path='editroom/:id' element={<EditRoom />} />
+                      <Route path='viewroom/:id' element={<ViewRoom />} />
                     </Route>
-                    <Route path="attendants" element={<RouteHome />}>
+                    <Route path='attendants' element={<RouteHome />}>
                       <Route index element={<AttendantListComponent />} />
-                      <Route path="add" element={<AddAttendant />} />
-                      <Route path="update/:id" element={<Update />} />
-                      <Route path="profile/:id" element={<Profile />} />
-                      <Route exact path="*" element={<ErrorComponent />} />
+                      <Route path='add' element={<AddAttendant />} />
+                      <Route path='update/:id' element={<Update />} />
+                      <Route path='profile/:id' element={<Profile />} />
+                      <Route exact path='*' element={<ErrorComponent />} />
                     </Route>
-                    <Route path="leave" element={<RouteHome />} >
+                    <Route path='leave' element={<RouteHome />} >
                       <Route index element={<LeaveList />} />
-                      <Route path="edit/:id" element={<EditRequest />} />
-                      <Route path="form" element={<LeaveForm />} />
+                      <Route path='edit/:id' element={<EditRequest />} />
+                      <Route path='form' element={<LeaveForm />} />
                     </Route>
                   </Route>
-                  {/* <Route exact path="/attendant" element={<HomeAttendant />} />  */}
+                  {/* <Route exact path='/attendant' element={<HomeAttendant />} />  */}
 
         </Routes>
         <Footer />
