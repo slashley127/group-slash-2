702ba63f6810cedb86d49f88c94e827363e2d690
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2024-02-17 20:26:54 -0600

    ccheck

--- roomranger/frontend/src/App.js
+++ roomranger/frontend/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/NavBar';
 import HomeRoom from './components/rooms/HomeRoom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AddRoom from './components/rooms/AddRoom';
-import EditRoom from './components/rooms/EditRoom';
-import ViewRoom from './components/rooms/ViewRoom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddRoom from "./components/rooms/AddRoom";
+import EditRoom from "./components/rooms/EditRoom";
+import ViewRoom from "./components/rooms/ViewRoom";
 import AssignRoom from './components/manager/AssignRoomForm';
 import EditAssignedRoom from './components/manager/EditAssignedRoom';
 import AttendantListComponent from './components/roomAttendants/AttendantListComponent';
@@ -13,69 +13,83 @@ import AddAttendant from './components/roomAttendants/AddAttendant';
 import Update from './components/roomAttendants/Update';
 import Profile from './components/roomAttendants/Profile';
 import { Footer } from './components/Footer';
-import LoginPage from './components/loginPage/LoginPage';
+import LoginPage from "./components/loginPage/LoginPage";
 import HomeManager from './components/manager/HomeManager';
 import ErrorComponent from './components/ErrorComponent';
 import RouteHome from './components/Route';
 import LeaveForm from './components/leaveRequest/LeaveForm';
 import LeaveList from './components/leaveRequest/LeaveList';
 import EditRequest from './components/leaveRequest/EditRequest';
-// import Login from './components/users/Login';
-// import Home from './components/pages/Home';
-// import Register from './components/users/Register';
-// import HomeNav from './components/layout/Navbar';
-
-
+import Home from './components/hompage/Home';
+import Registration from './components/hompage/Registration';
+import About from './components/About';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.className = isDarkMode ? 'dark-theme' : 'light-theme';
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <div className="App">
+      <div className="mode-toggle" onClick={toggleTheme}>
+        <div className={`slider ${isDarkMode ? 'dark' : 'light'}`} />
+         <span className="mode-text">{isDarkMode ? 'Light' : 'Dark'}</span>
+      </div>
       <Router>
-        <Routes>
-          {/* <Route element={<HomeNav />}> */}
-          {/* <Route element={<RouteHome />}>
-              <Route path='/' element={<Home />} />
-              <Route path='login' element={<LoginPage />} />
-              <Route path='register' element={<Register />} /> 
-           </Route> */}
-          {/* </Route> */}
-          <Route path='/' element={<RouteHome />}>
-            {/* <Route index element={<Register />} /> */}
-            <Route path='login' element={<LoginPage />} />
-          </Route>
-          <Route path="/landing" element={<NavBar />} > 
-            {/* <Route path='assignedrooms' element={<RouteHome />}> */}
-              <Route index element={<HomeManager />} />
-              <Route path="assignroomform" element={<AssignRoom />} />
-              <Route exact path="editassignedroom/:id" element={<EditAssignedRoom />} />
-            {/* </Route> */}
-            <Route path="rooms" element={<RouteHome />}>
-              <Route index element={<HomeRoom />} />
-              <Route path="addroom" element={<AddRoom />} />
-              <Route path="editroom/:id" element={<EditRoom />} />
-              <Route path="viewroom/:id" element={<ViewRoom />} />
-            </Route>
-            <Route path="attendants" element={<RouteHome />}>
-              <Route index element={<AttendantListComponent />} />
-              <Route path="add" element={<AddAttendant />} />
-              <Route path="update/:id" element={<Update />} />
-              <Route path="profile/:id" element={<Profile />} />
-              <Route exact path="*" element={<ErrorComponent />} />
-            </Route>
-            <Route path="leave" element={<RouteHome />} >
-              <Route index element={<LeaveList />} />
-              <Route path="edit/:id" element={<EditRequest />} />
-              <Route path="form" element={<LeaveForm />} />
-            </Route>
-          </Route>
-          {/* <Route exact path="/attendant" element={<HomeAttendant />} />  */}
+         <Routes>
+                  {/* <Route element={<HomeNav />}> */}
+                  {/* <Route element={<RouteHome />}>
+                      <Route path='/' element={<Home />} />
+                      <Route path='login' element={<LoginPage />} />
+                      <Route path='register' element={<Register />} />
+                   </Route> */}
+                  {/* </Route> */}
+                  {/* <Route path='/' element={<RouteHome />}> */}
+                    <Route path ='/'element={<Home />} />
+                    <Route path='Register' element={<Registration />} />
+                    <Route path='login' element={<LoginPage />} />
+                    <Route path='about' element={<About />} />
+                  {/* </Route> */}
+                  <Route path="/landing" element={<NavBar />} >
+                    {/* <Route path='assignedrooms' element={<RouteHome />}> */}
+                      <Route index element={<HomeManager />} />
+                      <Route path="assignroomform" element={<AssignRoom />} />
+                      <Route exact path="editassignedroom/:id" element={<EditAssignedRoom />} />
+                    {/* </Route> */}
+                    <Route path="rooms" element={<RouteHome />}>
+                      <Route index element={<HomeRoom />} />
+                      <Route path="addroom" element={<AddRoom />} />
+                      <Route path="editroom/:id" element={<EditRoom />} />
+                      <Route path="viewroom/:id" element={<ViewRoom />} />
+                    </Route>
+                    <Route path="attendants" element={<RouteHome />}>
+                      <Route index element={<AttendantListComponent />} />
+                      <Route path="add" element={<AddAttendant />} />
+                      <Route path="update/:id" element={<Update />} />
+                      <Route path="profile/:id" element={<Profile />} />
+                      <Route exact path="*" element={<ErrorComponent />} />
+                    </Route>
+                    <Route path="leave" element={<RouteHome />} >
+                      <Route index element={<LeaveList />} />
+                      <Route path="edit/:id" element={<EditRequest />} />
+                      <Route path="form" element={<LeaveForm />} />
+                    </Route>
+                  </Route>
+                  {/* <Route exact path="/attendant" element={<HomeAttendant />} />  */}
 
         </Routes>
         <Footer />
       </Router>
-      {/* <Footer/> */}
+       {/* <Footer/> */}
     </div>
-
-  );
+      );
 }
 
 export default App;
--- roomranger/src/main/java/org/launchcode/roomranger/models/User.java
+++ roomranger/src/main/java/org/launchcode/roomranger/models/User.java
@@ -1,8 +1,10 @@
 package org.launchcode.roomranger.models;
 
 import jakarta.persistence.*;
-import java.util.Set;
-//CHANGED LONG ID TO INT ID
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
 @Entity
 public class User {
 
@@ -10,21 +12,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 40, message = "First name must be between 2 and 40 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 40, message = "Last name must be between 2 and 40 characters")
     private String lastName;
-    private String dob;
 
     private String email;
 
     @Column(nullable = false)
     @Basic(optional = false)
     private String password;
-
-    private String role; // New field for a single role
-
-    @ElementCollection
-    private Set<String> roles; // Set of roles
 
     private String confirmPassword;
 
@@ -85,28 +89,7 @@ public class User {
         this.lastName = lastName;
     }
 
-    public String getDob() {
-        return dob;
-    }
 
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
-    }
 
     public String getConfirmPassword() {
         return confirmPassword;
