a43ee968ccce1c49e33a0e6216404b029a1468ba
samatakarka <samata.karka@gmail.com>
2024-02-20 20:02:14 -0600

    changed remaining days in entity to 20

---
+++ roomranger/frontend/src/components/LogoutComponent.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function LogoutComponent(){
+    return(
+        <div className="container mt-4 d-flex justify-content-center align-items-center ">
+            <h1> You are logged out!!  </h1>
+            <div>
+            <span>
+            <h1>Thank you for using our App. Come back soon!</h1>
+            </span>
+            </div>
+        </div>
+    )
+}
+export default LogoutComponent
